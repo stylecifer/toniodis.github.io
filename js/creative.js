fade68ff16abf7361d8ad2d45a5226ff722b188c
VAntonIO <stylustonypro@gmail.com>
2017-01-25 16:45:16 -0300

    Trantando de incorcorar el reel de youtube a al portafololio

--- js/creative.js
+++ js/creative.js
@@ -60,5 +60,22 @@
             tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
         }
     });
+    $('.video').magnificPopup({
+      type: 'iframe',
+
+
+      iframe: {
+         markup: '<div class="mfp-iframe-scaler">'+
+                    '<div class="mfp-close"></div>'+
+                    '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+                    '<div class="mfp-title">Some caption</div>'+
+                  '</div>'
+      },
+      callbacks: {
+        markupParse: function(template, values, item) {
+         values.title = item.el.attr('title');
+        }
+      }
+    });
 
 })(jQuery); // End of use strict
