ddc26a4fa6fd5251b7efea3ad5e90768c0cb4bac
VAntonIO <stylustonypro@gmail.com>
2017-01-27 03:42:05 -0300

    incorporacion de botón Play Videoe con un link hacia youtube

--- js/creative.js
+++ js/creative.js
@@ -62,15 +62,13 @@
     });
     $('.video').magnificPopup({
       type: 'iframe',
-
-
       iframe: {
          markup: '<div class="mfp-iframe-scaler">'+
                     '<div class="mfp-close"></div>'+
-                    '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-                    '<div class="mfp-title">Some caption</div>'+
+                        '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+                    '<div class="mfp-title">Reel motiongraphic tonioDis 2016</div>'+
                   '</div>'
-      },
+              },
       callbacks: {
         markupParse: function(template, values, item) {
          values.title = item.el.attr('title');
