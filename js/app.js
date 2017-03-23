10f1fa869ca2a5c5de9d5c3db08c786ddd0c5cca
VAntonIO <stylustonypro@gmail.com>
2017-03-22 22:00:48 -0300

    Cambios en la barra de navegación e incorporación de un icono para el menú

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
 $(document).foundation()
+var icon = document.getElementById("icon");
+
+icon.onclick = function() {
+    if (this.className === "Icon") {
+        this.className = "Icon open";
+    }
+    else {
+        this.className = "Icon";
+    }
+};
     // $('#offCanvas a').on('click', function () {
     //     $('#offCanvas').foundation('close')
     // })
+    // sidebar toggle triggers
+
+    // $("#close-sidebar").click(function() {
+    //   $('#offCanvas').toggleClass('reveal-for-medium');
+    // });
+    //
+    // $("#open-sidebar").click(function() {
+    //   $('#offCanvas').toggleClass('reveal-for-medium');
+    // });
